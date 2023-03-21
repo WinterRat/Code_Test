b37024fc7aa10c0f7232e3fc63ad3f62ca6d4fc2
winterrat <126951066+WinterRat@users.noreply.github.com>
2023-03-21 23:14:52 +0900

    [Silver III] Title: 주유소, Time: 24 ms, Memory: 3588 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n; 
+
+    cin>>n;
+    vector<long long> dist(n-1);
+    vector<long long> cost(n);
+    for(int i=0; i<n-1; i++){
+        cin>>dist[i]; 
+    }
+    for(int j=0; j<n; j++){ 
+        cin>>cost[j]; 
+    }
+    long long now=cost[0];
+    long long tot_c=0;
+
+    for(int i=0; i<n-1; i++){
+        now = min(now,cost[i]);
+        tot_c += now*dist[i];
+    }
+    cout<<tot_c<<'\n';
+    return 0;
+}
