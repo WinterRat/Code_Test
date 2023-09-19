c44ebcd52e56e3dca358019ed4b50870bf8daae4
WinterRat <126951066+WinterRat@users.noreply.github.com>
2023-09-19 22:05:38 +0900

    [Gold III] Title: 내리막 길, Time: 132 ms, Memory: 42788 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,29 +1,33 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10000)
+from sys import stdin
+input = stdin.readline
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
 
-def dfs(x, y, m, n, graph, dp):
-    # (m, n)에 도달한 경우
-    if x == m-1 and y == n-1:
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+dp = [[-1]*m for _ in range(n)]
+
+def dfs(x, y):
+    # 도착점에 도달하면 1 반환
+    if x == n-1 and y == m-1:
         return 1
-    # 이미 방문한 경우
+
+    # 이미 계산된 dp 값이라면 해당 값을 바로 반환
     if dp[x][y] != -1:
         return dp[x][y]
+    
+    # 방문 표시 (현재 위치에서 시작하는 경로의 수를 0으로 초기화)
     dp[x][y] = 0
+    
     for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-        if 0 <= nx < m and 0 <= ny < n and graph[x][y] > graph[nx][ny]:
-            dp[x][y] += dfs(nx, ny, m, n, graph, dp)
+        nx = x + dx[i]
+        ny = y + dy[i]   
+
+        if 0 <= nx < n and 0 <= ny < m and board[nx][ny] < board[x][y]:
+            dp[x][y] += dfs(nx, ny)
+
     return dp[x][y]
 
-def main():
-    m, n = map(int, input().split())
-    graph = [list(map(int, input().split())) for _ in range(m)]
-    dp = [[-1] * n for _ in range(m)]
-    print(dfs(0, 0, m, n, graph, dp))
-
-if __name__ == "__main__":
-    main()
+print(dfs(0, 0))
